75b26e7b96e9d28479f3091703b52d353123893c
notorite <notorite3t@gmail.com>
2020-08-06 22:01:04 +0900

    User name added

--- TossCoin.java
+++ TossCoin.java
@@ -1,12 +1,19 @@
 import java.util.Random;
+import java.util.Scanner;
 public class TossCoin {
        public static void main(String[] args) {
        	      Random random = new Random();
+	      Scanner scan = new Scanner(System.in);
 	      int r;
 	      int numHeads = 0;
 	      int numTails = 0;
 	      int maxRound = 3;
 	      String[] sides = {"Heads", "Tails"};
+
+	      //greets 
+	      System.out.print("Who are you?\n> ");
+	      String inputStr = scan.next();
+	      System.out.println("Hello, " + inputStr + "!");
 
 	      //toss a coin
 	      System.out.println("Tossing a coin...");
